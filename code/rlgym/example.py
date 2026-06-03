5c0e2cee01c16e9cf0ba7deaff87c77f7b244ce4
David Paulino <david.plnmr@gmail.com>
2026-06-03 22:59:35 +0200

    Reduce VPB reward since he started ballchasing

--- code/rlgym/example.py
+++ code/rlgym/example.py
@@ -2,7 +2,7 @@ import argparse
 import os
 
 
-RUN_ON_SLURM = True # If I run this on Slurm, I set this to True to avoid issues with KBHit and GPU/CPU tensor checkpoints. If I run it locally, I set this to False to disable WandB logging and enable rendering.
+RUN_ON_SLURM = False # If I run this on Slurm, I set this to True to avoid issues with KBHit and GPU/CPU tensor checkpoints. If I run it locally, I set this to False to disable WandB logging and enable rendering.
 
 if RUN_ON_SLURM:
     import patch_kbhit # Patch pour éviter les problèmes de KBHit sur Slurm qui attendent une entrée clavier. Doit être importé avant rlgym_ppo.
@@ -69,7 +69,7 @@ def build_rlgym_v2_env():
     reward_fn = LogCombinedReward(
         (GoalReward(), 1500),
         (AdvancedTouchReward(touch_reward=0.3, acceleration_reward=1.0), 30), 
-        (VelocityPlayerToBallReward(include_negative_values=False), 5),
+        (VelocityPlayerToBallReward(include_negative_values=False), 0.2),
         (CloseRangeFaceBallReward(), 0.5),
         (VelocityBallToGoalRewardZS, 30, "VelocityBallToGoal_ZS"),
         (AerialDistanceReward(), 150),
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--timesteps-limit",
         type=int,
-        default=5_000_000_000,
+        default=3_000_000_000,
         help="Limite de timesteps pour l'entraînement."
     )
     args = parser.parse_args()
