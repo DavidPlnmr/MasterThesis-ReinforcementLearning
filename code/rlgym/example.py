5b79b69c393d396d50d0ccf8f2b51707a15ac7da
David Paulino <david.plnmr@gmail.com>
2026-06-11 16:52:25 +0200

    Added AerialDistanceReward + gamma increased

--- code/rlgym/example.py
+++ code/rlgym/example.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--timesteps-limit",
         type=int,
-        default=3_500_000_000,
+        default=5_000_000_000,
         help="Limite de timesteps pour l'entraînement."
     )
     args = parser.parse_args()
