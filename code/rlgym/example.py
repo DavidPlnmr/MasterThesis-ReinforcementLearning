8ed10c5c29f48d7cd628dfa51b91be9544eafc4b
David Paulino <david.plnmr@gmail.com>
2026-06-15 13:40:53 +0200

    Improved the writing + 12b5 limit ts

--- code/rlgym/example.py
+++ code/rlgym/example.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--timesteps-limit",
         type=int,
-        default=5_000_000_000,
+        default=12_500_000_000,
         help="Limite de timesteps pour l'entraînement."
     )
     args = parser.parse_args()
